fe859afb82bf7aefd1dad9d5fed7eff581c90ce5
MarcusS919 <45082048+MarcusS919@users.noreply.github.com>
2018-11-16 19:43:14 -0800

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public float speed;
+    private Waypoints Wpoints;
+    private int waypointIndex;
+
+    public Transform target;
+
+    public float awarenessRange;
+    public float distanceToTarget;
+    private float temprange;
+
+    public float pSpeed;
+
+    public bool spotted = false;
+
+    public float distance;
+
+    public GameObject Player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (PlayerController.tempSpeed < PlayerController.pspeed)
+        {
+            temprange = awarenessRange / 2;
+        }
+        else
+        {
+            temprange = awarenessRange;
+        }
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+        //check the distance to the player
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        //check if the enemy is aware of player, if not then patrol
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, (Player.transform.position - transform.position), distance);
+
+        if (distanceToTarget < temprange && hitInfo.collider != null && hitInfo.collider.CompareTag("Player"))
+        {
+
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+
+            spotted = true;
+
+            Chase();
+        }
+
+        else
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.green);
+
+            spotted = false;
+
+            Patrol();
+
+        }
+    }
+
+    void Chase()
+    {
+        //chase 
+        float distanceToTarget2 = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget2 < temprange)
+        {
+            Vector3 targetDir = target.position - transform.position;
+            float angle2 = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.AngleAxis(angle2, Vector3.forward);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180);
+
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+    }
+
+    void Patrol()
+    {
+        //patrol
+        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+
+        //change direction when moving between waypoints
+        Vector3 dir = Wpoints.waypoints[waypointIndex].position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+        Quaternion p = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, p, 180f);
+
+        if (Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            waypointIndex++;
+        }
+
+        if (waypointIndex == Wpoints.waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public static float tempSpeed;
+    public static float pspeed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        pspeed = speed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            tempSpeed = speed / 2;
+        }
+        else
+        {
+            tempSpeed = speed;
+        }
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * tempSpeed;
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+            //to kill the player
+            //   Destroy(gameObject);
+            transform.position = new Vector3(-7, -4, 0);
+    }
+
+}
